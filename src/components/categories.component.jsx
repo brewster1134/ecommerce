45d5f60a1612180ba8cb40089b4f5a27c999a4bd
Ryan Brewster <brewster1134@gmail.com>
2020-03-06 14:13:36 -0800

    basic category support

--- src/components/categories.component.jsx
+++ src/components/categories.component.jsx
@@ -3,12 +3,34 @@ import './categories.styles.sass'
 
 import Category from './category.component'
 
-const Categories = () => (
-  <div className='ecommerce__categories'>
-    <Category />
-    <Category />
-    <Category />
-  </div>
-)
+class Categories extends React.Component {
+  constructor() {
+    super()
+  }
+
+  state = {
+    categories: [
+      {
+        title: 'Men',
+        color: 'LightBlue'
+      }, {
+        title: 'Women',
+        color: 'LightPink'
+      }
+    ]
+  }
+
+  render() {
+    return (
+      <div className='ecommerce__categories'>
+      {
+        this.state.categories.map(({ title, color }, index) => (
+          <Category id={index} title={title} color={color} />
+        ))
+      }
+      </div>
+    )
+  }
+}
 
 export default Categories
--- src/components/category.component.jsx
+++ src/components/category.component.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
-const Category = () => (
-  <div className='ecommerce_category'>
-    Category!
+const Category = ({ id, title, color }) => {
+  return (
+    <div
+      className='ecommerce_category'
+      id={`cat_${id}`}
+      style={{backgroundColor: `${color}`}}
+    >
+    <div className='ecommerce__copy'>
+      <h1 className='ecommerce__title'>{title}</h1>
+      <p className='ecommerce__subtitle'>Shop Now</p>
+    </div>
   </div>
-)
+  )
+}
 
 export default Category
